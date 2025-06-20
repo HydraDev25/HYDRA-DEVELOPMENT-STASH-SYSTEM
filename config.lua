df44b7c4b49b80e58b45e604c4270c074aa110e0
HydraDev25 <bmw.bmv421@gmail.com>
2025-06-20 16:31:58 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.InventoryLocations = {
+    vector3(123.4, 456.7, 78.9), -- STASH LOCATİONS
+    vector3(200.1, 300.2, 50.0),
+}
+
+Config.OpenKey = 38 -- 38 = E
+
+Config.OpenDistance = 2.5 -- DİSTANCE
+
+Config.MaxWeight = 1000000
+
+Config.SlotCount = 5000 
